620499d9a3b3303b7b2ca48e3bf4ec1034e6b791
Peter <adelekepeterpierre@gmail.com>
2024-07-01 05:24:15 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,27 @@
+const myTime = document.getElementById("time");
+const myDay = document.getElementById("day");
+
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+function dayTime() {
+  let today = new Date();
+  let day = today.getDay();
+
+  const currentDay = days[day];
+  myDay.innerHTML = currentDay;
+
+  let hour = today.getUTCHours();
+  let minute = today.getUTCMinutes();
+  let seconds = today.getUTCSeconds();
+  myTime.innerHTML = ` ${hour}:${minute}:${seconds}`;
+}
+
+setInterval(dayTime, 100);
